46dfa3c82b2d8fbf490cbc9572dbac67311f8457
Diana Moldovan <dianamoldovann@yahoo.com>
2024-01-10 22:29:04 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <QApplication>
+#include "Interfata/GUI/GUI.h"
+#include "Interfata/Model/GUIModel.h"
+#include "Interfata/UI/UI.h"
+#include "Teste/Teste.h"
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    Teste teste;
+    teste.Ruleaza_toate_testele();
+
+    MoviesFileRepository repo{"/Users/dianamoldovan/CLionProjects/OOP/LAB_13-14_OOP/Repository/Repository.txt"};
+    FilmValidator val;
+    Service service{repo, val};
+
+    UI ui{service};
+    //ui.runUI();
+
+    GUI gui{service};
+    //gui.setWindowState(Qt::WindowMaximized);
+    gui.resize(1200,600);
+    //gui.show();
+
+    GUIModel gui_model{service};
+    gui_model.show();
+
+    return a.exec();
+}
